b81a74e4feacd6284b87d22b1fb816ede94f14c3
Casey Foster <c@sey.me>
2019-03-02 10:17:40 -0600

    Apply rules

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 module.exports = {
-  extends: ['airbnb', 'plugin:prettier/recommended'],
-  parser: 'babel-eslint',
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  parser: "babel-eslint",
   parserOptions: {
-    sourceType: 'module',
+    sourceType: "module",
     ecmaVersion: 9,
-    ecmaFeatures: {jsx: true}
+    ecmaFeatures: { jsx: true }
   },
-  env: {browser: true, es6: true, jest: true, node: true},
+  env: { browser: true, es6: true, jest: true, node: true },
   rules: {
-    'consistent-return': 'off',
-    curly: ['error', 'multi-line'],
-    'guard-for-in': 'off',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'jsx-a11y/accessible-emoji': 'off',
-    'jsx-a11y/alt-text': 'off',
-    'no-multi-assign': 'off',
-    'no-param-reassign': 'off',
-    'no-plusplus': 'off',
-    'no-restricted-syntax': 'off',
-    'no-use-before-define': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/no-array-index-key': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'react/prop-types': 'off'
+    "consistent-return": "off",
+    curly: ["error", "multi-line"],
+    "guard-for-in": "off",
+    "import/extensions": "off",
+    "import/no-unresolved": "off",
+    "jsx-a11y/accessible-emoji": "off",
+    "jsx-a11y/alt-text": "off",
+    "no-multi-assign": "off",
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "no-use-before-define": "off",
+    "react/jsx-filename-extension": "off",
+    "react/no-array-index-key": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/prop-types": "off"
   }
 };
