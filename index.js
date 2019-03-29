abe45deafc95c39dec6f0d4ac79f69a9a526c734
Casey Foster <c@sey.me>
2019-03-29 10:41:46 -0500

    Revert, 2.0.9

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ module.exports = {
     'react/jsx-one-expression-per-line': 'off',
     'react/no-array-index-key': 'off',
     'react/prop-types': 'off',
+    'require-await': 'error',
     'sort-imports': ['error', { ignoreDeclarationSort: true }],
     curly: ['error', 'multi-line'],
     radix: 'off'
