35a94206398639dc25852acfc38edd1ef0e24324
Casey Foster <c@sey.me>
2019-09-18 14:22:18 -0500

    Revert

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ module.exports = {
     'react/prop-types': 'off',
     'require-atomic-updates': 'off',
     'require-await': 'error',
-    'sort-imports': ['error', { ignoreCase: false }],
+    'sort-imports': ['error', { ignoreDeclarationSort: true }],
     curly: ['error', 'multi-line']
   },
   settings: { react: { version: 'latest' } }
